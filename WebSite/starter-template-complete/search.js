b62628f708b8c81f7d39bf53fb521b899e379462
Liam Cavanagh <liamca@microsoft.com>
2016-02-04 12:54:17 -0800

    Updated Web page

--- WebSite/starter-template-complete/search.js
+++ WebSite/starter-template-complete/search.js
@@ -1,31 +1,18 @@
-// Copyright (c) 2015 
-// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is /furnished to do so, subject to the following conditions:
-// The above copyright notice and this permission notice shall be included in /all copies or substantial portions of the Software.
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-
-// Data was provided by GrouLens - http://grouplens.org/datasets/hetrec-2011/
-// Please refer to this page for details on the licensing of this data
-// http://files.grouplens.org/datasets/hetrec2011/hetrec2011-movielens-readme.txt
- 
+var azureSearchQueryApiKey = "";
+var azureMLModelId = ""
+var azureMLBuildId = ""
 var inSearch = false;
-var apiKey = [Azure Search Service API Key];
-var azureSearchService = [Azure Search Service - Do not add .search.windows.net];
 
 function execSuggest()
 {
-	// Use the autosuggest to lookup viable movies
+	// Execute a search to lookup viable movies
 	var q = encodeURIComponent($("#q").val());
-	var searchAPI = "https://" + azureSearchService + ".search.windows.net/indexes/movies/docs?$top=12&$select=title,spanishTitle,recommendations,imdbPictureURL&api-version=2015-02-28&search=" + q;
+	var searchAPI = "https://azs-playground.search.windows.net/indexes/movies/docs?$top=12&$select=id,title,imdbPictureURL&api-version=2015-02-28&search=" + q;
 	inSearch= true;
     $.ajax({
         url: searchAPI,
         beforeSend: function (request) {
-            request.setRequestHeader("api-key", apiKey);
+            request.setRequestHeader("api-key", azureSearchQueryApiKey);
             request.setRequestHeader("Content-Type", "application/json");
             request.setRequestHeader("Accept", "application/json; odata.metadata=none");
         },
@@ -34,50 +21,36 @@ function execSuggest()
 			$( "#mediaContainer" ).html('');
 			for (var item in data.value)
 			{
+				var id = data.value[item].id;
 				var title = data.value[item].title;
 				var imageURL = data.value[item].imdbPictureURL;
-				var recommendations = data.value[item].recommendations;
-				$( "#mediaContainer" ).append( '<div class="col-md-4" style="text-align:center"><a href="javascript:void(0);" onclick="openMovieDetails(\'' + title + '\',\'' + recommendations.toString() + '\',\'' + item.toString() + '\');"><img src=' + imageURL + ' height=200><br><div style="height:100px"><b>' + title + '</b></a></div></div>' );
+				$( "#mediaContainer" ).append( '<div class="col-md-4" style="text-align:center"><a href="javascript:void(0);" onclick="openMovieDetails(\'' + title + '\',\'' + id + '\');"><img src=' + imageURL + ' height=200><br><div style="height:100px"><b>' + title + '</b></a></div></div>' );
 			}
 			inSearch= false;
         }
     });
 }
 
-function openMovieDetails(title, recs, id)
+function openMovieDetails(title, id)
 {
 	// Open the dialog with the recommendations
 	$("#modal-title").html(title);
 	$("#recDiv").html('Loading recommendations...');
 
-	var filter = '';
-	var recArray = recs.split(',');
-	if (recArray[0] != ""){
-		for (var i=0; i< recArray.length; i++)
-		{
-			if (i==0)
-				filter+= '&$filter=id eq \'' + recArray[i] + '\'';
-			else
-				filter+= ' or id eq \'' + recArray[i] + '\'';
+	var recommendatationAPI = "https://api.datamarket.azure.com/data.ashx/amla/recommendations/v2/ItemRecommend?$format=json&modelId='" + azureMLModelId + "'&numberOfResults=5&buildId=" + azureMLBuildId + "&includeMetadata=false&apiVersion='1.0'&itemIds='" + id + "'";
+
+	$.ajax({
+		url: recommendatationAPI,
+		beforeSend: function (request) {
+			request.setRequestHeader("Authorization", "Basic QWNjb3VudEtleTpIK0ZDVldETWZZbnpja2ZUa3pxNDlzT01aR2dFVDlVNFdqL2xCSHhZeStzPQ==");
+		},
+		type: "GET",
+		success: function (data) {
+			$("#recDiv").html('');
+			for (var item in data.d.results)
+				$( "#recDiv" ).append( '<p>' + data.d.results[item].Name + '</p>' );
 		}
-		var searchAPI = "https://" + azureSearchService + ".search.windows.net/indexes/movies/docs?api-version=2015-02-28&search=*&$select=id,title" + filter;
-		
-		$.ajax({
-			url: searchAPI,
-			beforeSend: function (request) {
-				request.setRequestHeader("api-key", apiKey);
-				request.setRequestHeader("Content-Type", "application/json");
-				request.setRequestHeader("Accept", "application/json; odata.metadata=none");
-			},
-			type: "GET",
-			success: function (data) {
-				$( "#recDiv" ).html('');
-				for (var item in data.value)
-					$( "#recDiv" ).append( '<p>' + data.value[item].title + '</p>' );
-			}
-		});
-	} else 
-		$("#recDiv").html("No recommendations available");
+	});
 
 	$('#myModal').modal('show');
 }
